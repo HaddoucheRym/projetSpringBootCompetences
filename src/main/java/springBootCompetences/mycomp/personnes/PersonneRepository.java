331a7236e39e4c7032860e55150ed90ade4267d1
HaddoucheRym <rymmihoub@gmail.com>
2022-12-01 11:07:10 +0100

    feat: ajout du CRUD personnes

---
+++ src/main/java/springBootCompetences/mycomp/personnes/PersonneRepository.java
@@ -0,0 +1,7 @@
+package springBootCompetences.mycomp.personnes;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface PersonneRepository extends MongoRepository<Personne, String> {
+}
