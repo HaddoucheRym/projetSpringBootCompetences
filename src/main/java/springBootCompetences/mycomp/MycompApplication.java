0f2eced1c0f5b7f08813716496fe290a88942a9f
HaddoucheRym <rymmihoub@gmail.com>
2022-12-01 09:54:29 +0100

    first commit

---
+++ src/main/java/springBootCompetences/mycomp/MycompApplication.java
@@ -0,0 +1,13 @@
+package springBootCompetences.mycomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycompApplication.class, args);
+	}
+
+}
